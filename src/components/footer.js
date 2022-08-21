b001ee40814d230d977adec197214c50553d2153
YOUR FULL NAMEomarali1997 <omar.aziz.9718@gmail.com>
2022-08-21 22:32:59 +0300

    done lab01

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+
+                <a href="https://www.facebook.com/omaralm7seery">Facebook</a>
+                <a href="0791406102">Phone</a>
+                <p>omar Ali</p>
+                <p> &copy; copyRights 2022 </p>
+
+            </div>
+        )
+    }
+}
+
+export default Footer;
---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HornedBeast from './components/HornedBeast';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+
+                <HornedBeast />
+                <HornedBeast />
+
+            </div>
+        )
+    }
+}
+
+export default Main;

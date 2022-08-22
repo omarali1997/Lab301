f5c305c64e1aaf26869b961737ae083a6348705a
YOUR FULL NAMEomarali1997 <omar.aziz.9718@gmail.com>
2022-08-22 23:53:31 +0300

    ..

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -5,19 +5,19 @@ class HornedBeast extends React.Component {
         return (
             <div>
 
-                <h2>Animals</h2>
-                <img src='http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg' title='UniWhal' alt='UniWhal'></img>
-                <p>A unicorn and a narwhal nuzzling their horns</p>
+                <h2> {this.props.title} </h2>
 
-                <h2>Animals</h2>
-                <img src='https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80' title='Rhino Family' alt='Rhino Family'></img>
-                <p>Parent rhino with two babies</p>
+                <p> {this.props.disc} </p>
 
-                <h2>Animals</h2>
-                <img src='https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg' title='Unicorn Head' alt='Unicorn Head'></img>
-                <p>Someone wearing a very silly unicorn head mask</p>
-
-
+                <img src={this.props.img} alt='image' title={this.props.title} ></img>
+                <style jsx>
+                    {`
+                        img {
+                            width: '600px';
+                            height: '400px'; 
+                        }
+                     `}
+                </style>
 
             </div>
         )

c64cbb78fbe3d3c1ec21d44491e12c2a32f78138
YOUR FULL NAMEomarali1997 <omar.aziz.9718@gmail.com>
2022-08-23 23:22:02 +0300

    uncomplete

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
+import data from './components/data.json';
+import SelectedBeast from './components/SelectedBeast';
 
 
 class App extends React.Component {
+
+  
   render() {
     return (
       <div>

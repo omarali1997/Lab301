81e728a8cfc72aba0851ac7363fee7859fc65acc
YOUR FULL NAMEomarali1997 <omar.aziz.9718@gmail.com>
2022-08-26 04:41:59 +0300

    done03

--- src/App.js
+++ src/App.js
@@ -2,16 +2,39 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
+import Data from './components/Data.json';
+import UserModal from './components/SelectedBeast';
+
 
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      show:false,
+      selBeast:{}
+    }
+  }
+  myFunction=(title)=>{
+    const selectedBeast=Data.find(beast => beast.title === title);
+    this.setState({
+      show:true,
+      selBeast:selectedBeast
+    })
+  }
+
+  handleClose=()=>{
+    this.setState({
+      show:false})
+  }
   render() {
     return (
       <div>
 
         <Header />
-        <Main />
+        <Main Data={Data} myFunction={this.myFunction}/>
         <Footer />
+        <UserModal showModal={this.state.show} handleClose={this.handleClose} selBeast={this.state.selBeast} />
 
       </div>
     )
@@ -19,3 +42,7 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+// \\wsl.localhost\Ubuntu\home\omar\demo\my-first-app\src\components\Data.json
---
+++ src/components/SelectedBeast.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Card from 'react-bootstrap/Card';
+
+
+
+
+
+class UserModal extends React.Component {
+    render() {
+        return (
+            //         <Button variant="primary" onClick={handleShow}>
+            //     Launch demo modal
+            //   </Button>
+            <>
+                <Modal show={this.props.showModal} onHide={this.props.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title >{this.props.selBeast.Title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Card >
+                            <Card.Title>{this.props.selBeast._id}</Card.Title>
+                            <Card.Title>
+                                {this.props.selBeast.id}
+                                {this.props.selBeast.title}
+                            </Card.Title>
+                            <Card.Body>
+                                <Card.Img variant="top" src={this.props.selBeast.image_url} alt="image" title={this.props.selBeast.title} />
+                                <Card.Text>
+                                    <p>keyword : {this.props.selBeast.keyword}</p>
+                                    <p>horns : {this.props.selBeast.horns}</p>
+                                    <p>{this.props.selBeast.description}</p>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.props.handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+        )
+    }
+}
+
+export default UserModal;
